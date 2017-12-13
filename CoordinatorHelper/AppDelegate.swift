f66bc0bc26fa9499553197b473abaee9dac64cb9
Briahas <briahas@gmail.com>
2017-12-13 11:03:48 +0200

    Initial Commit

---
+++ CoordinatorHelper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CoordinatorHelper
+//
+//  Created by Mike Kholomeev on 12/13/17.
+//  Copyright © 2017 NixSolutions. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CoordinatorHelper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CoordinatorHelper
+//
+//  Created by Mike Kholomeev on 12/13/17.
+//  Copyright © 2017 NixSolutions. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
